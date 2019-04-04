2f03844e9684d72b01c4557025e4e3f1bd3393d0
MaxBot <maxime.botta.dev@gmail.com>
2019-04-04 17:02:16 +0200

    3 pages done (just have to fix the two bottom last pages

--- vue/src/components/affiche/affiche.vue
+++ vue/src/components/affiche/affiche.vue
@@ -1,21 +1,24 @@
 <template>
-    <div class="post">
+    <div class="col-md post">
         <div class="poster">
             <img v-bind:src="getImage(post.poster_path)" class="allImg" /> 
         </div>
-        <h4 v-if="post.title">Titre: {{post.title}}</h4>
-        <h4 v-else>Titre: {{post.name}}</h4>
-        <p>Date: {{post.first_air_date}}</p>
+        <h4 v-if="post.title">{{post.title}}</h4>
+        <h4 v-else> {{post.name}}</h4>
+        <p class="annee" v-if="post.first_air_date"> {{getYear(post.first_air_date)}} </p>
+        <p class="annee" v-else> {{getYear(post.release_date)}} </p>
     </div>
 </template>
 
 <script>
 import { getImage } from '../../utils/getImage'
+import { getYear } from '../../utils/getYear'
 
 export default {
 
     methods:{
         getImage,
+        getYear
     },
     props : ["post"]
 }
