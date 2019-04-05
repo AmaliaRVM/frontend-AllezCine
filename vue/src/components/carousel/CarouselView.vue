f489ffd40484d6315af946056ece6cdf9177bb8b
Amalia <amaliarvm@gmail.com>
2019-04-05 13:50:41 +0200

    add bootstrap

--- vue/src/components/carousel/CarouselView.vue
+++ vue/src/components/carousel/CarouselView.vue
@@ -1,11 +1,9 @@
 <template>
-    <!-- <div id='truc'> -->
-        <Carousel :per-page="1" :mouse-drag="true">
-            <Slide v-for="(slide, index) in lists" :key="index">
-                <img id="toposter" :src="slide" alt="movie">
-            </Slide>
-        </Carousel>
-    <!-- </div> -->
+    <Carousel :per-page="1" :mouse-drag="true">
+        <Slide v-for="(slide, index) in lists" :key="index">
+            <img id="toposter" :src="slide" alt="movie">
+        </Slide>
+    </Carousel>  
 </template>
 
 <script>
