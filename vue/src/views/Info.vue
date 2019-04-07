080942227e2e3689e9877f3ac7252a2d5f85ad0e
Amalia <amaliarvm@gmail.com>
2019-04-07 21:27:35 +0200

    add router

--- vue/src/views/Info.vue
+++ vue/src/views/Info.vue
@@ -1,12 +1,38 @@
 <template>
     <div class="containermovie">
-        <h1>info movie</h1>
+        <h1>{{$route.query.page}}</h1>
+        <Displayinfo {{post.id}}/>
+        
     </div>
 </template>
 
 <script>
-export default {
+import Displayinfo from '../components/displayinfo/Displayinfo.vue'
+import axios from 'axios'
 
+export default {
+    components: {
+        Displayinfo,
+    },
+
+    data(){
+        return {
+            posts:[]
+        }
+    },
+
+    mounted() {
+        var self = this;
+        axios.get('https://api.themoviedb.org/3/discover/movie?api_key=2704afc9f60b8ac59b4f28b3a0252704&language=en-US&sort_by=popularity.desc')
+        .then( function(res){
+            self.posts=res.data;
+            // console.log('Data: ',res.data);
+        })
+        .catch( function(error){
+            throw('Error: ', error);
+        })
+    }
+    
 }
 </script>
 
