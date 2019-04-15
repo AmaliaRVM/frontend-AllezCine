35996a32a39fcffb478007fdbe7ce5e23034f071
Matt <meurant.m@gmail.com>
2019-04-15 15:16:53 +0200

    Add and modify
    Adding delete test page, modifying code

--- php/getdata.php
+++ php/getdata.php
@@ -4,8 +4,8 @@ require ('headersettings.php');
 require ('pdo.php');
 
 if (isset($_GET['movie_id'])){
-    $req= $pdo -> prepare ('SELECT * FROM `commentary_table` WHERE movie_id=:Id');
-    $req -> execute ([':Id' => htmlspecialchars($_GET['movie_id'])]);
+    $req= $pdo -> prepare ('SELECT * FROM `commentary_table` WHERE movie_id=:movieId');
+    $req -> execute ([':movieId' => htmlspecialchars($_GET['movie_id'])]);
 }
 $rows = array();
     while($r = $req->fetch(PDO::FETCH_ASSOC)){
--- php/php-wip/removecomment.php
+++ php/php-wip/removecomment.php
@@ -6,16 +6,16 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if //condition de suppression (push button) //{
-    $req= $pdo -> prepare ('DELETE IN`commentary_table`');
-    $req -> execute ([ ]);
+if (isset ($_GET['movie_id']) && isset ($_GET['id']) && isset ($_GET['delete_button'])){
+    $req= $pdo -> prepare ('DELETE FROM`commentary_table` WHERE id=:Id');
+    $req -> execute ([':Id' => htmlspecialchars($_GET['id'])]);
     if(!$req){
         print json_encode('req error');
     }else{
-        print json_encode($_GET);
+        print "Commentary removed";
     }
 }else{
-    echo 'error';
+    echo 'Can\'t delete!!';
 }
 
 ?>
